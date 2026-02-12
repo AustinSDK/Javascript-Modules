ef1fe7beb4da82074b23ff0ff40d051498948327
Austin Moore <austin@austinsdk.me>
2026-02-12 14:24:33 -0600

    Rename conversions.js to Conversions.js

---
+++ modules/Conversions.js
@@ -0,0 +1,12 @@
+/*
+ * Created by Austin
+ * https://cdn.jsdelivr.net/gh/AustinSDK/Javascript-Modules@latest/modules/Conversions.js
+*/
+function hexToString(hexx) {
+  // Convert hex to decimal number directly
+  return parseInt(hexx, 16).toString();
+}
+
+const rgbToHex = (r, g, b) => `#${[r, g, b].map(
+  v => v.toString(16).padStart(2, '0')
+).join('')}`;
